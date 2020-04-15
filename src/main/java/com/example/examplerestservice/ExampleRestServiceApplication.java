ac8d6fe0b4289ad9c02bb05a7e5e9b284117ff4f
Kamil Sopata <kamil.sopata@northpass.com>
2020-04-15 19:17:37 +0200

    Initial commit

---
+++ src/main/java/com/example/examplerestservice/ExampleRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.examplerestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleRestServiceApplication.class, args);
+	}
+
+}
